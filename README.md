bd0ca93ba012c32fe4722c17b54f183b21077045
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-07-19 15:18:31 +0330

    Upload Exercises-part-1-20 Project On Github

---
+++ README.md
@@ -0,0 +1,29 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 20
+
+Programming Language ===> Python
+
+Program description ===> 20th Exercise
+
+Example ===> Write A Program That Shows The Difference Between The Following Two Lists
+
+List A => a = [1, 2, 3, 4, 5]
+
+List B => b = [4, 5, 6, 7, 8]
+
+Description Of Files:
+
+practice_20_linux.py ===> For Linux 
+
+For Run ===> python3 practice_20_linux.py
+
+practice_20_windows.py ===> For Windows
+
+For Run ===> python practice_20_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ===> To install => pip install termcolor2
+
+pyfiglet ===> To install => pip install pyfiglet
---
+++ practice_20_windows.py
@@ -0,0 +1,33 @@
+# Practice_20
+
+# Library
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('cls')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 20', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+# Value 
+a = [1, 2, 3, 4, 5]
+b = [4, 5, 6, 7, 8]
+
+# Convert List To Set
+a = set(a)
+b = set(b)
+
+# Computing
+print(f'a-b ===> {a-b}')
+print('===============')
+print(f'b-a ===> {b-a}')
+
+
+# Finish
+# Create By Moein Heshmati
